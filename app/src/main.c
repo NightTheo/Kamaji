a11fd65c5edd3e59323285710b6f588e0dfa805d
Nigth <omnes.theo@gmail.com>
2020-11-29 18:07:59 +0100

    Error: can't load the glade file

--- app/src/main.c
+++ app/src/main.c
@@ -16,22 +16,43 @@ gcc `pkg-config --cflags gtk+-3.0` -o exe/main src/main.c `pkg-config --libs gtk
 #include <gtk/gtk.h>
 #include "config_mysql.c"
 
+void homekamaji(int argcounter, char **argvalue);
 
 int main(int argc, char **argv){
 
   MYSQL *conn = connect_db();
   MYSQL_RES *res;
   res = query(conn, "show tables");
-
   print_result(res);
-
   mysql_close(conn);
-  return 0;
+
+  GtkBuilder      *builder;
+  GtkWidget       *window;
+
+  gtk_init(&argc, &argv);
+
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file (builder, "glade/home.glade", NULL);
+
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "window_home"));
+  if (window == NULL){
+    /* Print out the error. You can use GLib's message logging  */
+    fprintf(stderr, "Unable to file object with id \"window_home\" \n");
+    //exit(1);
+    /* Your error handling code goes here */
+  }
+  gtk_builder_connect_signals(builder, NULL);
+
+  g_object_unref(builder);
+
+  gtk_widget_show(window);
+  gtk_main();
 }
 /*END MAIN___________________________________________*/
 
-
-
-
+void on_window_home_destroy()
+{
+    gtk_main_quit();
+}
 
 //
