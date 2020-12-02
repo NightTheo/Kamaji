1075a0166c245417bfc47d762e3c34860006c0b3
Nigth <omnes.theo@gmail.com>
2020-12-02 09:43:09 +0100

    home -> reservations home -> new res -> equipment home -> place&room -> planning

--- app/inc/import_glade.h
+++ app/inc/import_glade.h
@@ -6,9 +6,13 @@ last modif: 30 nov 2020
 
 void open_home_window(char *idWindow);
 
-void open_new_res_window(GtkWidget *Widget,GtkWidget *window);
-void open_reservations_window(GtkWidget *Widget,GtkWidget *window);
-void open_place_room_window(GtkWidget *Widget,GtkWidget *window);
+GtkBuilder *close_and_open_window(GtkWidget *oldWindow, char *idNewWindow);
+
+void open_reservations_window(GtkWidget *Widget,GtkWidget *currentWindow);
+void open_new_res_window(GtkWidget *Widget,GtkWidget *currentWindow);
+void open_place_room_window(GtkWidget *Widget,GtkWidget *currentWindow);
+void open_equipment_window(GtkWidget *Widget,GtkWidget *currentWindow);
+void open_planning_window(GtkWidget *Widget,GtkWidget *currentWindow);
 
 void on_window_main_destroy();
 void on_window_new_booking_destroy();
--- app/src/import_glade.c
+++ app/src/import_glade.c
@@ -46,10 +46,29 @@ void open_home_window(char *idWindow){
 
 }
 
+/*
+click_button
+*/
 void click_button(GtkBuilder *builder, GtkWidget *window, char *idButton, void (*pf)){
   GtkWidget *button;
   button = GTK_WIDGET(gtk_builder_get_object(builder, idButton));
   g_signal_connect (button,"clicked",G_CALLBACK(pf),window);
+}
+
+/*
+close_and_open_window
+*/
+GtkBuilder *close_and_open_window(GtkWidget *oldWindow, char *idNewWindow){
+  GtkBuilder      *builder;
+  gtk_widget_destroy(oldWindow);
+
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file (builder,GLADE_FILE, NULL);
+  oldWindow = GTK_WIDGET(gtk_builder_get_object(builder, idNewWindow));
+  gtk_builder_connect_signals(builder, NULL);
+
+  gtk_widget_show(oldWindow);
+  return builder;
 }
 
 
@@ -60,44 +79,32 @@ void click_button(GtkBuilder *builder, GtkWidget *window, char *idButton, void (
 function: open_new_res_window
 Open window_new_reservation
 */
-void open_new_res_window(GtkWidget *Widget,GtkWidget *window){
-    GtkBuilder      *builder;
-    gtk_widget_destroy(window);
-
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file (builder,GLADE_FILE, NULL);
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_new_reservation"));
-    gtk_builder_connect_signals(builder, NULL);
-
-    g_object_unref(builder);
-    gtk_widget_show(window);
+void open_reservations_window(GtkWidget *Widget,GtkWidget *currentWindow){
+  close_and_open_window(currentWindow,"window_reservations");
 }
 
-void open_reservations_window(GtkWidget *Widget,GtkWidget *window){
-    GtkBuilder      *builder;
-    gtk_widget_destroy(window);
+void open_new_res_window(GtkWidget *Widget,GtkWidget *currentWindow){
+  GtkBuilder *builder;
+  builder = close_and_open_window(currentWindow,"window_new_reservation");
+  click_button(builder, currentWindow, "button_new_res", &open_equipment_window);
+  g_object_unref(builder);
+}
 
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file (builder,GLADE_FILE, NULL);
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_reservations"));
-    gtk_builder_connect_signals(builder, NULL);
+void open_place_room_window(GtkWidget *Widget,GtkWidget *currentWindow){
+  GtkBuilder *builder;
+  builder = close_and_open_window(currentWindow,"window_place_room");
+  click_button(builder, currentWindow, "button_place_room", &open_planning_window);
+  g_object_unref(builder);
+}
 
-    g_object_unref(builder);
-    gtk_widget_show(window);
+void open_equipment_window(GtkWidget *Widget,GtkWidget *currentWindow){
+    close_and_open_window(currentWindow,"window_equipment");
+}
+
+void open_planning_window(GtkWidget *Widget,GtkWidget *currentWindow){
+    close_and_open_window(currentWindow,"window_planning");
 }
 
 
 
-void open_place_room_window(GtkWidget *Widget,GtkWidget *window){
-    GtkBuilder      *builder;
-    gtk_widget_destroy(window);
-
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file (builder,GLADE_FILE, NULL);
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_place_room"));
-    gtk_builder_connect_signals(builder, NULL);
-
-    g_object_unref(builder);
-    gtk_widget_show(window);
-}
 //#############
